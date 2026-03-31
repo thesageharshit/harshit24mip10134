f3af7dfc5c633086e574c3809bff2137eccfa4e3
thesageharshit <harshit.24mip10134@vitbhopal.ac.in>
2026-03-31 23:32:25 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 3: Disk and Permission Auditor
-# Author: Aryawart Gupta
+# Author: Harshit sagwaliya
 # Course: Open Source Software
 
 echo "========================================="
