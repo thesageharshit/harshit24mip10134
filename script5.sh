dc6dbca2a38d208ab4ce236dacc605d6cec33575
thesageharshit <harshit.24mip10134@vitbhopal.ac.in>
2026-03-31 23:33:25 +0530

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 5: Open Source Manifesto Generator
-# Author: Aryawart Gupta
+# Author: Harshit sagwaliya
 # Course: Open Source Software
 
 echo "========================================="
